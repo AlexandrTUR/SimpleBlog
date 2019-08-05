8560ee48aeec1a1771bc48619cf497ae9c390979
AlexandrTUR <valeralox7@mail.ru>
2019-08-05 15:06:00 +0300

    Initial commit

---
+++ Models/UsesrsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBlog.Models
+{
+    public class UsesrsModel
+    {
+        public string name;
+        public string surname;
+        public int age;
+        public bool isHasCare = true;
+
+    }
+}

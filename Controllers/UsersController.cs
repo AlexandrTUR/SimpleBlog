24c2a18e944c27b5112f9acf42d9e71c162450b4
AlexandrTUR <valeralox7@mail.ru>
2019-08-06 15:37:28 +0300

    add new page, new controller

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using SimpleBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListUsers()
+        {
+            List<UsersModel> users = new List<UsersModel>();
+            users.Add(new UsersModel("Tom", "Jarky", 22, false));
+            users.Add(new UsersModel("Mickle", "Jarky", 25, false));
+            users.Add(new UsersModel("Jone", "Marley", 31, true));
+            users.Add(new UsersModel("Alex", "Jarky", 12, false));
+
+
+            return View(users);
+        }
+    }
+}
---
+++ Models/UsersModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBlog.Models
+{
+    public class UsersModel
+    {
+        public string name;
+        public string surname;
+        public int age;
+        public bool isHasCare = true;
+
+        public UsersModel(string name, string surname, int age, bool isHasCare) {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+            this.isHasCare = isHasCare;
+        } 
+    }
+}
